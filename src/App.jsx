06ffff89e678a204401e53e138ad10dd4eb29eec
tavolafourcade <tavolafourcade@gmail.com>
2022-11-05 21:01:31 -0500

    [ ADD ] Vite implementation with a todo list basic application

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([])
+
+  const handleAddTodo = (e) => {
+    e.preventDefault()
+    const text = e.target["text"].value
+
+    if(!text || todos.includes(text)) return
+    setTodos(todos => todos.concat(text))
+  }
+
+  const handleRemoveTodo = (index) => {
+    setTodos(todos => todos.filter((todo, _index) => _index !== index))
+  }
+  return (
+    <div>
+      <form onSubmit={handleAddTodo}>
+        <input type="text" name="text"/>
+        <button>Add todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => {
+          return <li key={index}>{todo}<button onClick={() => handleRemoveTodo(index)}>X</button></li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default App
